a1b0b44a294efc0f6ed50f25e0bbd06dfaf334cd
Francisco Santiago de la Cruz <44105867+franciscosantiagoc@users.noreply.github.com>
2023-09-05 22:54:53 -0600

    Preparando estructura inicial del proyecto

---
+++ src/app/gifs/components/search-form/search-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-form',
+  templateUrl: './search-form.component.html'
+})
+export class SearchFormComponent {
+
+}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.less']
+})
+export class SidebarComponent {
+
+}
