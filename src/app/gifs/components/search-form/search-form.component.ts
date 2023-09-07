72833a551244576f7d52c2c87e33797ee08afa45
Francisco Santiago de la Cruz <44105867+franciscosantiagoc@users.noreply.github.com>
2023-09-06 22:38:46 -0600

    Uso e implementacion de la directiva ViewChild para referenciar al html

--- src/app/gifs/components/search-form/search-form.component.ts
+++ src/app/gifs/components/search-form/search-form.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-form',
   templateUrl: './search-form.component.html'
 })
 export class SearchFormComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
 
+  searchTag(/* newTag: string<-- forma nativa */):void {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag})
+  }
 }
